30e9a028f1338b69c6ad1488ac4d30c1244cfe03
Sushant <vettel@pop-os.localdomain>
2020-05-02 16:05:43 +0530

    added callback

--- src/Hello.js
+++ src/Hello.js
@@ -1,27 +1,8 @@
-import React, { useRef, useState, useEffect, useLayoutEffect } from "react";
-import { useFetch } from "./useFetch";
-import { useMeasure } from "./useMeasure";
+import React from "react";
+// import { useCountRenders } from "./useCountRenders";
 
-export const Hello = () => {
-  // const renders = useRef(0);
-  const [count, setCount] = useState(() =>
-    JSON.parse(localStorage.getItem("count"))
-  );
-  const { data, loading } = useFetch(`http://numbersapi.com/${count}/trivia`);
-  useEffect(() => {
-    localStorage.setItem("count", JSON.stringify(count));
-  }, [count]);
+export const Hello = React.memo(({ increment }) => {
+  // useCountRenders();
 
-  const [rect, divRef] = useMeasure([data]);
-
-  return (
-    <div>
-      <div style={{ display: "flex" }}>
-        <div ref={divRef}>{!data ? "loading..." : data}</div>
-      </div>
-      <pre>{JSON.stringify(rect, null, 2)}</pre>
-      <div>count: {count}</div>
-      <button onClick={() => setCount((c) => c + 1)}>increment</button>
-    </div>
-  );
-};
+  return <button onClick={() => increment(5)}>hello</button>;
+});
